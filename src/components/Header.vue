00a1f703ed31e419e0e42e10050409206df2c58d
jiayong <36148963@qq.com>
2019-05-11 17:43:45 +0800

    增加跳转

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -9,13 +9,13 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#"><span class="glyphicon glyphicon-console" aria-hidden="true"></span> Qblog</a>
+          <a class="navbar-brand" href="#/"><span class="glyphicon glyphicon-console" aria-hidden="true"></span> Qblog</a>
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
           <ul class="nav navbar-nav">
-            <li class="active"><a href="#">首页 <span class="sr-only">(current)</span></a></li>
+            <li class="active"><a href="#/">首页 <span class="sr-only">(current)</span></a></li>
             <li class="dropdown">
               <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">分类 <span class="caret"></span></a>
               <ul class="dropdown-menu">
@@ -61,7 +61,7 @@ export default {
   },
   methods: {
     toRegister: function () {
-      alert('注册')
+      window.location.hash = '#/register'
     }
   }
 }
